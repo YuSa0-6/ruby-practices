de5e5ca80f0e4a49ddb77fde75f780c037a3cbcb
YuSa0-6 <fuku.sugar@icloud.com>
2023-07-04 16:24:34 +0900

    [update] 引数からできるように修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,21 +2,18 @@ require 'optparse'
 require 'Date'
 
 options = ARGV.getopts('y:', 'm:')
-year = options[":y"].to_i
-month = options[":m"].to_i
-year = Date.today.year if options[":y"].nil?
-month = Date.today.month if options[":m"].nil?
+year = options["y"].nil? ? Date.today.year : options["y"].to_i
+month = options["m"].nil? ? Date.today.month : options["m"].to_i
 
 puts "      #{month}月 #{year}"
 puts "日 月 火 水 木 金 土"
 
 first_wday = Date.new(year, month, 1).wday
-first_wday.times { print "   " }
+print "   " * first_wday
 last_day = Date.new(year, month, -1).day
 next_sat = 7 - first_wday
 
-## 右に空白が出力されるのでcenterを使う
-## 参考：　https://docs.ruby-lang.org/ja/latest/method/String/i/center.html
+# 参考：　https://docs.ruby-lang.org/ja/latest/method/String/i/center.html
 (1..last_day).each do |d|
   if d == next_sat
     next_sat += 7
