84301db959f3fd1317119ee4714fbba6e99ba2dd
YuSa0-6 <fuku.sugar@icloud.com>
2023-07-04 17:10:42 +0900

    [update]

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,7 +2,7 @@ require 'optparse'
 require 'Date'
 
 options = ARGV.getopts('y:', 'm:')
-year = options["y"].nil? ? Date.today.year : options["y"].to_i
+year  = options["y"].nil? ? Date.today.year  : options["y"].to_i
 month = options["m"].nil? ? Date.today.month : options["m"].to_i
 
 puts "      #{month}月 #{year}"
@@ -13,13 +13,14 @@ print "   " * first_wday
 last_day = Date.new(year, month, -1).day
 next_sat = 7 - first_wday
 
-# 参考：　https://docs.ruby-lang.org/ja/latest/method/String/i/center.html
+# 参考：https://docs.ruby-lang.org/ja/latest/method/String/i/center.html
 (1..last_day).each do |d|
+  day = d.to_s.center(3)
   if d == next_sat
     next_sat += 7
-    puts d.to_s.center(3)
+    puts day
   else
-    print d.to_s.center(3)
+    print day
   end
 end
 puts ""
