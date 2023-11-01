7a4386861080eb0d193511c4a5476c058114b75b
YuSa0-6 <fuku.sugar@icloud.com>
2023-11-01 20:28:36 +0900

    add option -a

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,21 +1,30 @@
 # frozen_string_literal: true
 
-def main(row, path)
+require 'optparse'
+
+def main(row)
+  path = set_paramater
   files = find_files(path)
   formatted_files = format_files(files)
   vertical_lines = sort_files(formatted_files, row)
   display_files(vertical_lines, row)
 end
 
+def set_paramater
+  path = '*'
+  option = OptionParser.new
+  option.on('-a') { path = '{.,*}{.,*}' }
+  option.parse!(ARGV)
+  path
+end
+
 def find_files(path)
   Dir.glob(path)
 end
-
 def format_files(files)
   most_long_name = files.max_by(&:length)
   files.map { |file| file.ljust(most_long_name.length + 4) }
 end
-
 def sort_files(formatted_files, row)
   sorted_files = formatted_files.sort
   vertical_lines = []
@@ -27,9 +36,8 @@ def sort_files(formatted_files, row)
   end
   vertical_lines
 end
-
 def display_files(vertical_lines, row)
   vertical_lines.each_with_index { |file, index| print ((index + 1) % row).zero? ? "#{file}\n" : file }
 end
 
-main(3, '*')
+main(3)
