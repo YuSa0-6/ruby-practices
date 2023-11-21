0861adace363f908e8f04fd87582ecc946f01780
YuSa0-6 <fuku.sugar@icloud.com>
2023-11-21 15:59:14 +0900

    [update] create -r option

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,23 +3,20 @@
 require 'optparse'
 
 def main(row)
-  path = set_paramater
-  files = find_files(path)
+  files = find_files
   formatted_files = format_files(files)
-  vertical_lines = sort_files(formatted_files, row)
-  display_files(vertical_lines, row)
+  sorted_files = sort_files(formatted_files, row)
+  display_files(sorted_files, row)
 end
 
-def set_paramater
+def find_files
   path = '*'
+  r_option = false
   option = OptionParser.new
   option.on('-a') { path = '{.,*}{.,*}' }
+  option.on('-r') { r_option = true }
   option.parse!(ARGV)
-  path
-end
-
-def find_files(path)
-  Dir.glob(path)
+  r_option ? Dir.glob(path).reverse : Dir.glob(path)
 end
 
 def format_files(files)
@@ -28,19 +25,18 @@ def format_files(files)
 end
 
 def sort_files(formatted_files, row)
-  sorted_files = formatted_files.sort
-  vertical_lines = []
-  number_of_lines = (sorted_files.length.to_f / row).ceil
+  sorted_files = []
+  number_of_lines = (formatted_files.length.to_f / row).ceil
   number_of_lines.times do |col_index|
     row.times do |row_index|
-      vertical_lines << sorted_files[col_index + number_of_lines * row_index]
+      sorted_files << formatted_files[col_index + number_of_lines * row_index]
     end
   end
-  vertical_lines
+  sorted_files
 end
 
-def display_files(vertical_lines, row)
-  vertical_lines.each_with_index { |file, index| print ((index + 1) % row).zero? ? "#{file}\n" : file }
+def display_files(sorted_files, row)
+  sorted_files.each_with_index { |file, index| print ((index + 1) % row).zero? ? "#{file}\n" : file }
 end
 
 main(3)
