20a6e8b4238e41a241d88efc19c1e48dd7b45f3b
YuSa0-6 <fuku.sugar@icloud.com>
2024-03-01 18:23:45 +0900

    [delete][create] 標準入力が来た時に処理を分けるようにする

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -4,68 +4,36 @@ require 'optparse'
 require 'debug'
 
 def main
-  params = fetch_params
-  file_paths = fetch_paths
-  display_result(file_paths, params)
+  pp params = fetch_params
+  results = count_results(paths: ARGV)
 end
 
 def fetch_params
   opt = OptionParser.new
-  params = { w: false, l: false, c: false }
-  opt.on('-w') { |v| params[:w] = v }
+  params = {}
   opt.on('-l') { |v| params[:l] = v }
+  opt.on('-w') { |v| params[:w] = v }
   opt.on('-c') { |v| params[:c] = v }
   opt.parse!(ARGV, into: params)
-  params = { w: true, l: true, c: true } if params.values.none?
-  params
+  params.empty? ? params = {l: true, w: true, c: true} : params 
 end
 
-def fetch_paths
-  file_paths = ARGV
-  file_paths = readlines.map { |v| v.scan(/(?!total)(?<=\s)[A-Za-z0-9]+.[A-Za-z0-9]+\n/) } if file_paths.empty?
-  file_paths.flatten.map(&:chomp)
-end
-
-def count_lines(text)
-  text.lines.count
-end
-
-def count_words(text)
-  text.split(/\s+/).size
-end
-
-def fetch_bytesize(path)
-  File::Stat.new(path).size
-end
-
-def display_total(total, params)
-  print total[:lines].to_s.rjust(8) if params[:l]
-  print total[:words].to_s.rjust(8) if params[:w]
-  print total[:bytesize].to_s.rjust(8) if params[:c]
-  print " total\n"
-end
-
-def init_total
-  { lines: 0, words: 0, bytesize: 0 }
-end
-
-def multiple_paths?(file_paths)
-  file_paths.count > 1
-end
-
-def display_result(file_paths, params)
-  total = init_total
-  file_paths.map do |path|
-    text = File.read(path)
-    print count_lines(text).to_s.rjust(8) if params[:l]
-    print count_words(text).to_s.rjust(8) if params[:w]
-    print fetch_bytesize(path).to_s.rjust(8) if params[:c]
-    puts " #{path}"
-    total[:lines] += count_lines(text)
-    total[:words] += count_words(text)
-    total[:bytesize] += fetch_bytesize(path)
+def count_results(paths:)
+  if paths.empty?
+    pp fetch_count(text: $stdin.read, path: '')
+  else
+    pp paths.map {|f| fetch_count(text: File.read(f), path: f)}
   end
-  display_total(total, params) if multiple_paths?(file_paths)
 end
+
+def fetch_count(text:, path:)
+  {
+    lines: text.lines.count,
+    words: text.split(/\s+/).size,
+    textsize: text.bytesize,
+    path: path
+  }
+end
+
 
 main
