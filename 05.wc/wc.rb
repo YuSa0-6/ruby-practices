0f906d7718833068e5c727f20c5028eabb6e46a2
YuSa0-6 <fuku.sugar@icloud.com>
2024-03-06 13:46:37 +0900

    [update] Rubocop通す

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -6,9 +6,9 @@ require 'debug'
 def main
   params = fetch_params
   paths = ARGV
-  results = count_results(paths: paths)
-  print_results(results: results, params: params)
-  print_total(results: results) if multi_path?(results: results) 
+  results = count_results(paths:)
+  print_results(results:, params:)
+  print_total(results:, params:) if multi_path?(results:)
 end
 
 def fetch_params
@@ -25,7 +25,7 @@ def count_results(paths:)
   if paths.empty?
     [fetch_counts(text: $stdin.read, path: '')]
   else
-    paths.map { |path| fetch_counts(text: File.read(path), path: path) }
+    paths.map { |path| fetch_counts(text: File.read(path), path:) }
   end
 end
 
@@ -34,7 +34,7 @@ def fetch_counts(text:, path:)
     lines: text.lines.count,
     words: text.split(/\s+/).size,
     textsize: text.bytesize,
-    path: path
+    path:
   }
 end
 
@@ -52,15 +52,15 @@ def multi_path?(results:)
 end
 
 def print_total(results:)
-  totals = count_totals(results: results)
-  print totals[:lines].to_s.rjust(8)
-  print totals[:words].to_s.rjust(8)
-  print totals[:textsize].to_s.rjust(8)
+  totals = count_totals(results:)
+  print totals[:lines].to_s.rjust(8) if params[:l]
+  print totals[:words].to_s.rjust(8) if params[:w]
+  print totals[:textsize].to_s.rjust(8) if params[:c]
   print " total\n"
 end
 
 def count_totals(results:)
-  totals = {lines: 0, words: 0, textsize: 0}
+  totals = { lines: 0, words: 0, textsize: 0 }
   results.map do |result|
     totals[:lines] += result[:lines]
     totals[:words] += result[:words]
