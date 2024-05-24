59b988f0fd0a623f31f75faadf484b9681cce618
YuSa0-6 <fuku.sugar@icloud.com>
2024-05-24 16:30:23 +0900

    ボウリングのスコアを計算する。 - フレームごとに分割し、最後の３フレームについては３つにせずにそのまま２つづつに分割する - 計算の判定としては   - double_strike   - single_strike   - spare   - else   の通り。   のこり３つの特典に関しては加点を考える必要がないためそのまま加算する。

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -1,39 +1,40 @@
+require './frame'
 class Game
   def initialize
-    @marks = ARGV[0].split(',')
-    @shots = []
-    @frames = []
+    p @marks = ARGV[0].split(',')
+    p @frames = create_frames.map { Frame.new(_1, _2)}
     @total_score = 0
   end
 
-  def game_count
+  def create_frames
+    replaced_marks = []
     @marks.each do |mark|
       if mark == 'X'
-        @shots << 10
-        @shots << 0
+        replaced_marks << 'X'
+        replaced_marks << '0'
       else
-        @shots << mark.to_i
+        replaced_marks << mark
       end
     end
-    
-    @shots.each_slice(2) { |shot| @frames << shot }
+    replaced_marks.each_slice(2).to_a.map
+  end
 
-    @frames.each_with_index do |frame, index|
-      next_frame = @frames[index + 1]
-      next_after_frame = @frames[index + 2]
-      p @total_score += if frame[0] == 10 && next_frame[0] == 10 #double strike
-                        10 + 10 + next_after_frame[0]
-                      elsif frame[0] == 10 #single strike
-                        10 + next_frame.sum
-                      elsif frame.sum == 10 #spare
-                        10 + next_frame[0]
-                      else
-                        frame.sum
-                      end
-
-                      break if index == 9
+  def game_count
+    @frames.each_with_index do |frame, idx|
+      next_frame = @frames[idx+1]
+      next_after_frame = @frames[idx+2]
+      p @total_score += if frame.double_strike?(next_frame)
+        20 + next_after_frame.first_shot_score
+      elsif frame.strike?
+        10 + next_frame.score
+      elsif frame.spare?
+        10 + next_frame.first_shot_score
+      else
+        frame.score
+      end
+      break if idx == 9
     end
   end
 end
 
-p Game.new.game_count
+Game.new.game_count
