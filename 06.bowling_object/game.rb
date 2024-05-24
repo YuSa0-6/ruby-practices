65175e66d5d5861ec454565fd2d65a40195443cb
YuSa0-6 <fuku.sugar@icloud.com>
2024-05-24 13:10:06 +0900

    topで動作することを確認する。

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -1,9 +1,39 @@
 class Game
   def initialize
-    @frames = 10.times.map { Frame.new(first_mark, second_mark, third_mark) }
+    @marks = ARGV[0].split(',')
+    @shots = []
+    @frames = []
+    @total_score = 0
   end
 
   def game_count
-    @frames.size
+    @marks.each do |mark|
+      if mark == 'X'
+        @shots << 10
+        @shots << 0
+      else
+        @shots << mark.to_i
+      end
+    end
+    
+    @shots.each_slice(2) { |shot| @frames << shot }
+
+    @frames.each_with_index do |frame, index|
+      next_frame = @frames[index + 1]
+      next_after_frame = @frames[index + 2]
+      p @total_score += if frame[0] == 10 && next_frame[0] == 10 #double strike
+                        10 + 10 + next_after_frame[0]
+                      elsif frame[0] == 10 #single strike
+                        10 + next_frame.sum
+                      elsif frame.sum == 10 #spare
+                        10 + next_frame[0]
+                      else
+                        frame.sum
+                      end
+
+                      break if index == 9
+    end
   end
 end
+
+p Game.new.game_count
