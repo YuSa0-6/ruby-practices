e5039f9a44202ea848e3f299e6c645b584981c94
YuSa0-6 <fuku.sugar@icloud.com>
2024-05-24 16:27:10 +0900

    フレーム内でのオブジェクトを格納するために、作成する

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -1,11 +1,28 @@
+require './shot'
 class Frame
-  def initialize(first_mark, second_mark, third_mark)
-    @first_shot = Shot.new(first_mark))
-    @second_shot = Shot.new(second_mark)
-    @third_shot = Shot.new(third_mark)
+  MAX_PINS = 10
+  def initialize(first_mark, second_mark)
+    @first_mark = first_mark
+    @second_mark = second_mark
   end
 
   def score
-    [@first_shot, @second_shot, @third_shot].sum
+    [Shot.new(@first_mark).count_pins, Shot.new(@second_mark).count_pins].sum
+  end
+
+  def first_shot_score
+    Shot.new(@first_mark).count_pins
+  end
+
+  def double_strike?(next_frame)
+    strike? && next_frame.strike?
+  end
+
+  def strike?
+    @first_mark == 'X'
+  end
+
+  def spare?
+    Shot.new(@first_mark).count_pins + Shot.new(@second_mark).count_pins == 10
   end
 end
