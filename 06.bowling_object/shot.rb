bbf3d9675ed94aaece0b588a78f0b659dff3a1d8
YuSa0-6 <fuku.sugar@icloud.com>
2024-05-31 13:02:57 +0900

    strike?をShotクラスで管理する。

--- 06.bowling_object/shot.rb
+++ 06.bowling_object/shot.rb
@@ -2,12 +2,16 @@
 
 class Shot
   STRIKE_MARK = 'X'
-  
+
   def initialize(mark)
     @mark = mark
   end
 
   def count_pins
     @mark == STRIKE_MARK ? 10 : @mark.to_i
+  end
+
+  def strike?
+    @mark == STRIKE_MARK
   end
 end
